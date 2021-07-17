a247b91f2a95cb5beb4fb077ea6288733a41decd
Руслан Чекушко <chekushkor@gmail.com>
2021-07-17 14:24:14 +0300

    --

--- 03. Lists (Списки)/03.01.name.py
+++ 03. Lists (Списки)/03.01.name.py
@@ -2,3 +2,22 @@ bicycles = ['trek', 'cannondale', 'reline', 'specialized']
 message = f"My first bicycles was a {bicycles[0].title()}"
 
 print(message)
+
+# Добавление элементов в список
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.append('iz')
+print(motorcycles)
+
+# Удаление элемента из списка по индексу
+del motorcycles[0]
+print(motorcycles)
+
+# Удаление элемента с использованием pop()
+popped_motorcycle = motorcycles.pop()
+print(popped_motorcycle)
+
+# Удаление элемента из списка по значению
+motorcycles.remove('suzuki')
+print(motorcycles)
