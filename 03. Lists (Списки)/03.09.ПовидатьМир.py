5330bfe8e2d48d58d08116183ac7940640d72eb1
Чекушко Руслан <chekushkor@gmail.com>
2021-07-19 19:53:47 +0300

    --

---
+++ 03. Lists (Списки)/03.09.ПовидатьМир.py
@@ -0,0 +1,17 @@
+# ----------------------------------------
+# Created by Ruslan.C.
+# 
+#
+#   Version   Date
+#   1.0      17.07.2021
+#   Description
+#
+# ----------------------------------------
+
+# Повидать мир
+
+country_lists = ['сша', 'индия', 'тайланд', 'африка', 'япония']
+len_list = len(country_lists)
+
+print("Here is the original list:")
+print(country_lists)
